c98b8a1727061197039b86cd008d7f02e11c21a8
SuvajitMajhi <suvajit.majhi19.work@gmail.com>
2024-09-05 11:21:25 +0530

    done

--- src/main/lab.py
+++ src/main/lab.py
@@ -19,4 +19,11 @@
                 variable as the result.
 
 """
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count = count +1
+    return count
 
